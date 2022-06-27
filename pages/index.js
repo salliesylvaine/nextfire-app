29412e2965c4dd64992cc4c0c186d1203dea46d2
Sallie McKee <salliemckee7@gmail.com>
2022-06-27 09:16:23 -0500

    added toast

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,13 @@ import Link from "next/link";
 import Loader from "../components/Loader";
 import styles from "../styles/Home.module.css";
 
+import toast from "react-hot-toast";
+
 export default function Home() {
   return (
     <div>
       <Loader show />
+      <button onClick={() => toast.success("hello toast")}>Toast Me</button>
     </div>
   );
 }
