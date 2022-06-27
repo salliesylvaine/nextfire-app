168954e9c286074716f2fadeb9b618fa52cd9cd4
Sallie McKee <salliemckee7@gmail.com>
2022-06-27 10:09:43 -0500

    added google sign in button and changed firebase imports to comply with v.9

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,41 @@
-export default function EnterPage({}) {
+import { auth, googleAuthProvider } from "../lib/firebase";
+
+export default function Enter(props) {
+  const user = null;
+  const username = null;
+
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
 }
+
+//sign in with google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} /> Sign in with Google
+    </button>
+  );
+}
+
+//sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+//user selects username
+function UsernameForm() {}
