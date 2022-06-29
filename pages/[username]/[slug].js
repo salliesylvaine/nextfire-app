a09b14a3df510de4ca53068ee74657f582afe333
Sallie McKee <salliemckee7@gmail.com>
2022-06-29 09:28:05 -0500

    added PostContent component and styling and realtime data hydration

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,7 +1,7 @@
-// import styles from "../../styles/Post.modules.css";
-// import PostContent from "../../components/PostContent";
+import styles from "../../styles/Post.module.css";
+import PostContent from "../../components/PostContent";
 import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
-// import { useDocumentData } from "react-firebase-hooks/firestore";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 
 export async function getStaticProps({ params }) {
   const { username, slug } = params;
@@ -41,5 +41,22 @@ export async function getStaticPaths() {
 }
 
 export default function Post(props) {
-  return <main></main>;
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+
+  return (
+    <main className={styles.container}>
+      <section>
+        <PostContent post={post} />
+      </section>
+
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🤍</strong>
+        </p>
+      </aside>
+    </main>
+  );
 }
