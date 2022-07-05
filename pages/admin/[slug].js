8e08c9bc4aa0a3eb10cf210dc89c6d51025d4f56
Sallie McKee <salliemckee7@gmail.com>
2022-07-05 10:16:10 -0500

    transforming error messages from useForm

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -11,6 +11,7 @@ import toast from "react-hot-toast";
 import styles from "../../styles/Admin.module.css";
 import AuthCheck from "../../components/AuthCheck";
 import { firestore, auth } from "../../lib/firebase";
+
 //importing directly from Firestore, issues when trying to export it from lib/firebase
 import { serverTimestamp } from "firebase/firestore";
 
@@ -70,10 +71,19 @@ function PostManager() {
 
 //defaultValues = data from firestore document
 function PostForm({ defaultValues, postRef, preview }) {
-  const { register, handleSubmit, reset, watch } = useForm({
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState,
+    formState: { errors },
+  } = useForm({
     defaultValues,
     mode: "onChange",
   });
+
+  const { isValid, isDirty } = formState;
 
   const updatePost = async ({ content, published }) => {
     await postRef.update({ content, published, updatedAt: serverTimestamp() });
@@ -91,7 +101,17 @@ function PostForm({ defaultValues, postRef, preview }) {
         </div>
       )}
       <div className={preview ? styles.hidden : styles.controls}>
-        <textarea {...register("content")}></textarea>
+        <textarea
+          {...register(
+            "content",
+            { maxLength: { value: 20000, message: "content is too long" } },
+            { minLength: { value: 10, message: "content is too short" } },
+            { required: { value: true, message: "content is required" } }
+          )}
+        ></textarea>
+        {errors.content && (
+          <p className="text-danger">{errors.content.message}</p>
+        )}
         <fieldset>
           <input
             className={styles.checkbox}
@@ -101,7 +121,11 @@ function PostForm({ defaultValues, postRef, preview }) {
           <label>Published</label>
         </fieldset>
 
-        <button type="submit" className="btn-green">
+        <button
+          type="submit"
+          className="btn-green"
+          disabled={!isDirty || !isValid}
+        >
           Save Changes
         </button>
       </div>
