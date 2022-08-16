2201c42978ba7904886208e9e8531c8a2e523884
Sallie McKee <salliemckee7@gmail.com>
2022-08-16 12:21:44 -0500

    added notes to postfeed

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -11,6 +11,7 @@ export default function PostFeed({ posts, admin }) {
 function PostItem({ post, admin = false }) {
   //Naive method to calc word count and read time
   const wordCount = post?.content.trim().split(/\s+/g).length;
+  //assuming user can read 100 words per minute
   const minutesToRead = (wordCount / 100 + 1).toFixed(0);
 
   return (
