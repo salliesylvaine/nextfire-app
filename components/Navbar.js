3a1daed730135aaa3870acc8f5c7e2cafb1e28c4
Sallie McKee <salliemckee7@gmail.com>
2022-08-25 11:10:45 -0500

    changed feed button to home button in navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
       <ul>
         <li>
           <Link href="/">
-            <button className="btn-logo">FEED</button>
+            <button className="btn-logo">HOME</button>
           </Link>
         </li>
         {/* {user is signed in and has username} */}
